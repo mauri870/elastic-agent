c8eae1f45a156f0b6ac028c149da2277266d788d
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2026-01-20 13:27:28 -0300

    more tests

--- internal/edot/otelcol/metrics_test.go
+++ internal/edot/otelcol/metrics_test.go
@@ -259,6 +259,248 @@ service:
 	require.Empty(t, cmp.Diff(expected, ev), "metrics do not match expected values")
 }
 
+func TestMonitoringTelemetryMetricsFailed(t *testing.T) {
+	cfg := `receivers:
+  elasticmonitoringreceiver:
+    interval: 1s
+exporters:
+  debug:
+    verbosity: detailed
+  elasticsearch/1:
+    mapping:
+      mode: bodymap
+    endpoints:
+      - {{.ESEndpoint}}
+    max_conns_per_host: 1
+    retry:
+      enabled: true
+      initial_interval: 1s
+      max_interval: 1m0s
+      max_retries: 1
+    sending_queue:
+      batch:
+        flush_timeout: 10s
+        max_size: 1600
+        min_size: 0
+        sizer: items
+      block_on_overflow: true
+      enabled: true
+      num_consumers: 1
+      queue_size: 3200
+      wait_for_result: true
+
+service:
+  pipelines:
+    logs:
+      receivers: [elasticmonitoringreceiver]
+      exporters:
+        #- debug
+        - elasticsearch/1
+`
+
+	monitoringReceived := make(chan mapstr.M, 1)
+
+	var eventCount int
+	failedEvents := make(map[string]struct{})
+	deterministicHandler := func(action api.Action, event []byte) int {
+		var curEvent mapstr.M
+		require.NoError(t, json.Unmarshal(event, &curEvent))
+		if _, ok := failedEvents[curEvent["@timestamp"].(string)]; ok {
+			return http.StatusTooManyRequests
+		}
+
+		if ok, _ := curEvent.HasKey("beat.stats"); ok && eventCount > 3 {
+			monitoringReceived <- curEvent
+			return http.StatusOK
+		}
+
+		eventCount++
+		failedEvents[curEvent["@timestamp"].(string)] = struct{}{}
+		return http.StatusTooManyRequests
+	}
+
+	esURL := integration.StartMockESDeterministic(t, deterministicHandler)
+
+	configParams := struct {
+		ESEndpoint string
+	}{
+		ESEndpoint: esURL,
+	}
+
+	var configBuffer bytes.Buffer
+	require.NoError(t,
+		template.Must(template.New("config").Parse(cfg)).Execute(&configBuffer, configParams),
+	)
+
+	settings := NewSettings("test", []string{"yaml:" + configBuffer.String()})
+
+	// enable exporter metrics
+	featuregate.GlobalRegistry().Set("telemetry.newPipelineTelemetry", true)
+
+	collector, err := otelcol.NewCollector(*settings)
+	require.NoError(t, err)
+	require.NotNil(t, collector)
+
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
+	wg := startCollector(ctx, t, collector, "")
+	defer func() {
+		cancel()
+		collector.Shutdown()
+		wg.Wait()
+	}()
+
+	var ev mapstr.M
+	select {
+	case ev = <-monitoringReceived:
+		require.NotNil(t, ev, "monitoring event should not be nil")
+	case <-time.After(30 * time.Second):
+		t.Fatal("timeout waiting for monitoring event")
+	}
+
+	ev = ev.Flatten()
+	require.NotEmpty(t, ev["@timestamp"], "expected @timestamp to be set")
+	ev.Delete("@timestamp")
+	require.Greater(t, ev["beat.stats.libbeat.output.write.bytes"], float64(0))
+	ev.Delete("beat.stats.libbeat.output.write.bytes")
+
+	expected := mapstr.M{
+		"beat.stats.libbeat.pipeline.queue.max_events":    float64(3200),
+		"beat.stats.libbeat.pipeline.queue.filled.events": float64(0),
+		"beat.stats.libbeat.pipeline.queue.filled.pct":    float64(0),
+		"beat.stats.libbeat.output.events.total":          float64(4),
+		"beat.stats.libbeat.output.events.active":         float64(0),
+		"beat.stats.libbeat.output.events.acked":          float64(4),
+		"beat.stats.libbeat.output.events.dropped":        float64(0),
+		"beat.stats.libbeat.output.events.batches":        float64(8),
+		"beat.stats.libbeat.output.events.failed":         float64(4),
+		"component.id": "elasticsearch/1",
+	}
+
+	require.Empty(t, cmp.Diff(expected, ev), "metrics do not match expected values")
+}
+
+func TestMonitoringTelemetryMetricsDropped(t *testing.T) {
+	cfg := `receivers:
+  elasticmonitoringreceiver:
+    interval: 1s
+exporters:
+  debug:
+    verbosity: detailed
+  elasticsearch/1:
+    mapping:
+      mode: bodymap
+    endpoints:
+      - {{.ESEndpoint}}
+    compression: none
+    max_conns_per_host: 1
+    retry:
+      enabled: false
+    sending_queue:
+      batch:
+        flush_timeout: 1s
+        max_size: 1600
+        min_size: 0
+        sizer: items
+      block_on_overflow: true
+      enabled: true
+      num_consumers: 1
+      queue_size: 3200
+      wait_for_result: true
+
+service:
+  pipelines:
+    logs:
+      receivers: [elasticmonitoringreceiver]
+      exporters:
+        #- debug
+        - elasticsearch/1
+`
+
+	monitoringReceived := make(chan mapstr.M, 1)
+
+	var eventCount int
+	failedEvents := make(map[string]struct{})
+	deterministicHandler := func(action api.Action, event []byte) int {
+		var curEvent mapstr.M
+		require.NoError(t, json.Unmarshal(event, &curEvent))
+		if _, ok := failedEvents[curEvent["@timestamp"].(string)]; ok {
+			return http.StatusBadRequest
+		}
+
+		if ok, _ := curEvent.HasKey("beat.stats"); ok && eventCount > 3 {
+			monitoringReceived <- curEvent
+			return http.StatusOK
+		}
+
+		eventCount++
+		failedEvents[curEvent["@timestamp"].(string)] = struct{}{}
+		return http.StatusBadRequest
+	}
+
+	esURL := integration.StartMockESDeterministic(t, deterministicHandler)
+
+	configParams := struct {
+		ESEndpoint string
+	}{
+		ESEndpoint: esURL,
+	}
+
+	var configBuffer bytes.Buffer
+	require.NoError(t,
+		template.Must(template.New("config").Parse(cfg)).Execute(&configBuffer, configParams),
+	)
+
+	settings := NewSettings("test", []string{"yaml:" + configBuffer.String()})
+
+	// enable exporter metrics
+	featuregate.GlobalRegistry().Set("telemetry.newPipelineTelemetry", true)
+
+	collector, err := otelcol.NewCollector(*settings)
+	require.NoError(t, err)
+	require.NotNil(t, collector)
+
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
+	wg := startCollector(ctx, t, collector, "")
+	defer func() {
+		cancel()
+		collector.Shutdown()
+		wg.Wait()
+	}()
+
+	var ev mapstr.M
+	select {
+	case ev = <-monitoringReceived:
+		require.NotNil(t, ev, "monitoring event should not be nil")
+	case <-time.After(30 * time.Second):
+		t.Fatal("timeout waiting for monitoring event")
+	}
+
+	ev = ev.Flatten()
+	require.NotEmpty(t, ev["@timestamp"], "expected @timestamp to be set")
+	ev.Delete("@timestamp")
+	require.Greater(t, ev["beat.stats.libbeat.output.write.bytes"], float64(0))
+	ev.Delete("beat.stats.libbeat.output.write.bytes")
+
+	expected := mapstr.M{
+		"beat.stats.libbeat.pipeline.queue.max_events":    float64(3200),
+		"beat.stats.libbeat.pipeline.queue.filled.events": float64(0),
+		"beat.stats.libbeat.pipeline.queue.filled.pct":    float64(0),
+		"beat.stats.libbeat.output.events.total":          float64(4),
+		"beat.stats.libbeat.output.events.active":         float64(0),
+		"beat.stats.libbeat.output.events.acked":          float64(4),
+		"beat.stats.libbeat.output.events.dropped":        float64(4),
+		"beat.stats.libbeat.output.events.batches":        float64(4),
+		// "beat.stats.libbeat.output.events.failed":         float64(0), // omitted if zero
+		"component.id": "elasticsearch/1",
+	}
+
+	require.Empty(t, cmp.Diff(expected, ev), "metrics do not match expected values")
+}
+
 func TestMetricsTelemetry(t *testing.T) {
 	tests := []struct {
 		name                      string
